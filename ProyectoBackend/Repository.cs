374a1515652c21600d7b291527fa89d82df31be9
Saul Padilla <saulpadilla.e@gmail.com>
2023-06-25 03:01:26 -0600

    Se agrega una lista de cervezerias y metodos para obtener toda la lista y un elemento en especifico

---
+++ ProyectoBackend/Repository.cs
@@ -0,0 +1,23 @@
+﻿namespace ProyectoBackend
+{
+    public class Repository
+    {
+        List<Brewery> breweries = new List<Brewery>()
+        {
+            new Brewery() { Id = 1, Name = "Minerv" },
+            new Brewery() { Id = 2, Name = "Beaver" },
+            new Brewery() { Id = 3, Name = "Samichlaus" },
+        };
+        public List<Brewery> GetBreweries() => breweries;
+        public Brewery? GetBrewery(int id) => breweries.Find(p=> p.Id == id); //Compara el ID en el metodo con el ID de la lista
+     
+
+
+    }
+
+    public class Brewery
+    {
+    public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
