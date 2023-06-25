3578d8e36ed09566f3fcf23b53c669f9d305f0d4
Saul Padilla <saulpadilla.e@gmail.com>
2023-06-25 03:15:22 -0600

    Se agregan los endpoint de los url "brewery" para acceder a toda la lista o a un elemento respectivamente

--- ProyectoBackend/Program.cs
+++ ProyectoBackend/Program.cs
@@ -1,6 +1,8 @@
+using ProyectoBackend;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/brewery", () => new Repository().GetBreweries());
 
 app.Run();
