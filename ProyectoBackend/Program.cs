9bb611200df36db83a3033e401849d7edf79100d
Saul Padilla <saulpadilla.e@gmail.com>
2023-06-29 11:37:01 -0600

    Se mejora la calidad del codigo usando un contexto global de la base de datos

--- ProyectoBackend/Program.cs
+++ ProyectoBackend/Program.cs
@@ -2,6 +2,7 @@ using ProyectoBackend;//Importa el namespace de la clase Repository
 using ProyectoBackend.Models;
 
 var builder = WebApplication.CreateBuilder(args);//Crea la aplicacion
+builder.Services.AddDbContext<PubContext>();//Agrega el contexto de la base de datos    
 var app = builder.Build();//Crea la aplicacion
 
 app.MapGet("/", () => "Hello World!");
@@ -13,16 +14,7 @@ app.MapGet("/brewery/{id}", (int id) =>   //Devuelve la cerveceria con el ID que
     return brewery is not null ? Results.Ok(brewery) : Results.NotFound();//Si la cerveceria no es nula devuelve la cerveceria, si es nula devuelve un error 404
 });
 
-app.MapGet("/beers", () =>
-{
-    List<Beer> beers = null; //Crea una lista de cervezas
+app.MapGet("/beers", (PubContext db) => db.Beers.ToList()); //Devuelve la lista de cervezas
 
-    using (var db = new PubContext()) //Crea un contexto de la base de datos
-    {
-        beers = db.Beers.ToList(); //Obtiene la lista de cervezas de la base de datos
-    }
-
-    return beers; //Devuelve la lista de cervezas
-});
 
 app.Run(); //Ejecuta la aplicacion
